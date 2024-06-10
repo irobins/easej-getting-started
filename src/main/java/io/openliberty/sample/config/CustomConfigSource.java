61d32c8864c4024cb2cb3f50a390daeb4984e538
Alasdair Nottingham <alasdair.nottingham@gmail.com>
2024-06-10 11:53:17 -0400

    Update CustomConfigSource.java

--- src/main/java/io/openliberty/sample/config/CustomConfigSource.java
+++ src/main/java/io/openliberty/sample/config/CustomConfigSource.java
@@ -49,6 +49,7 @@ public class CustomConfigSource implements ConfigSource {
 
   public Map<String, String> getProperties() {
     Map<String, String> m = new HashMap<String, String>();
+    /**
     String jsonData = this.readFile(this.fileLocation);
     JsonParser parser = Json.createParser(new StringReader(jsonData));
     String key = null;
@@ -77,6 +78,7 @@ public class CustomConfigSource implements ConfigSource {
       }
     }
     parser.close();
+    */
     return m;
   }
 
